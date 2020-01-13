64a00b18e78a4e6e867b7e1757bc9c845922bb94
Kadyr Poyraz <poyraz_2001@mail.ru>
2020-01-13 11:58:13 +0200

    Готово

--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore,applyMiddleware} from "redux";
+import thunkMiddleware from 'redux-thunk';
+import reducer from "./store/reducer";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux'
+import {saveCounter} from "./store/action";
+
+export const store = createStore(reducer,applyMiddleware(thunkMiddleware));
+
+store.subscribe(() => {
+    saveCounter(store.getState().counter)
+});
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+
+);
+
+ReactDOM.render(app, document.getElementById('root'));
